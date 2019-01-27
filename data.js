9979eea374fcd930d3f7758e12a3b377c32a7531
satyamounika11 <msmounica1192@gmail.com>
2019-01-27 23:36:46 +0530

    withAPI

--- data.js
+++ data.js
@@ -1,3 +1,11 @@
+var app = angular.module('myApp', []);
+app.controller('pokemonCtrl', function($scope, $http) {
+  $http.get("https://dev-util.edyst.com/challenge/pokemons/random").then(function (response) {
+      $scope.myData = response.data;
+  });
+});
+
+
 /*var app = angular.module('myApp', [["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
